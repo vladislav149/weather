ce8d01f82152ef76f0742ecef71e3d8794440b10
vladislav149 <vladislav149@mail.ru>
2022-03-07 01:42:12 +0300

    finished HTML coding & js nav now

--- js/main.js
+++ js/main.js
@@ -16,3 +16,24 @@ document.querySelectorAll('.main__tab').forEach(item =>
 );
 
 document.querySelector('.main__tab').click();
+
+
+const button = document.querySelector('.main__search');
+button.addEventListener('click', sendCity);
+
+async function sendCity() {
+  const inputValue = document.querySelector('.main__input').value;
+  const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+  const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+  const url = `${serverUrl}?q=${inputValue}&appid=${apiKey}`;
+  let response = await fetch(url);
+  if (response.ok) {
+    let json = await response.json();
+    const city = document.querySelector('.main__city');
+    const temperature = document.querySelector('.main__temperature');
+    city.textContent = inputValue[0].toUpperCase() + inputValue.slice(1);
+    temperature.textContent = Math.round(json.main.temp - 271) + '°';
+  } else {
+    alert("Ошибка HTTP: " + response.status);
+  }
+}

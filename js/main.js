3054fa039f47dafbef761adaa204c1c6b47d98a4
vladislav149 <vladislav149@mail.ru>
2022-03-10 01:49:33 +0300

    finished 59 task

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 import {
   UI_ELEMENTS,
   showInfo,
-  resetInput
+  resetInput,
+  addFavoriteCite,
+  deleteFavoriteCity,
+  favoriteCity
+
 } from './view.js'
 
 const SERVER = {
@@ -10,6 +14,14 @@ const SERVER = {
 }
 
 UI_ELEMENTS.FORM_SUBMIT.addEventListener('submit', getWeather);
+UI_ELEMENTS.BUTTON_LIKE.addEventListener('click', changeListFavoriteCity);
+UI_ELEMENTS.CITY_BUTTON.forEach(item => {
+  item.addEventListener('click', showThisCity)
+});
+UI_ELEMENTS.CITY_DELETE_BUTTON.forEach(item => {
+  item.addEventListener('click', deleteCity)
+});
+
 
 function getWeather(e) {
   e.preventDefault();
@@ -33,4 +45,37 @@ function getJSON() {
       }
     })
     .catch(err => alert('Ошибка url', err))
+}
+
+function changeListFavoriteCity() {
+  this.classList.toggle('main__btn-heart--active');
+  const nameCity = this.previousElementSibling.textContent;
+  let isThereACity = favoriteCity.findIndex(elem => elem === nameCity)
+  if (isThereACity + 1) {
+    favoriteCity.splice(isThereACity, 1)
+    deleteFavoriteCity(nameCity)
+    console.log(favoriteCity);
+  } else {
+    favoriteCity.push(nameCity)
+    addFavoriteCite(nameCity)
+    const newFavoriteCity = UI_ELEMENTS.LIST_FAVORITE_CITY.lastChild.querySelector('.main__btn');
+    newFavoriteCity.addEventListener("click", showThisCity);
+    const deleteNewFavoriteCity = UI_ELEMENTS.LIST_FAVORITE_CITY.lastChild.querySelector('.main__delete');
+    deleteNewFavoriteCity.addEventListener("click", deleteCity);
+    console.log(favoriteCity);
+  }
+}
+
+function showThisCity() {
+  UI_ELEMENTS.INPUT_CITY.value = this.textContent;
+  showInfo(getJSON());
+  resetInput();
+}
+
+function deleteCity() {
+  const city = this.previousElementSibling.textContent;
+  deleteFavoriteCity(city);
+  let isThereACity = favoriteCity.findIndex(elem => elem === city)
+  favoriteCity.splice(isThereACity, 1);
+  UI_ELEMENTS.BUTTON_LIKE.classList.remove('main__btn-heart--active');
 }
--- js/view.js
+++ js/view.js
@@ -4,8 +4,14 @@ export const UI_ELEMENTS = {
   FORM_SUBMIT: document.querySelector('.main__form'),
   TEMPERATURE: document.querySelector('.main__temperature'),
   CITY: document.querySelectorAll('.main__city'),
-  INPUT_CITY: document.querySelector('.main__input')
+  INPUT_CITY: document.querySelector('.main__input'),
+  BUTTON_LIKE: document.querySelector('.main__btn-heart'),
+  LIST_FAVORITE_CITY: document.querySelector('.main__list'),
+  CITY_BUTTON: document.querySelectorAll('.main__btn'),
+  CITY_DELETE_BUTTON: document.querySelectorAll('.main__delete')
 }
+
+export const favoriteCity = ['Amur Oblast', 'Samara Oblast', 'Bali', 'Dane', 'Kilo', 'Nur-Sultan'];
 
 UI_ELEMENTS.TABS_BUTTON.forEach(item =>
   item.addEventListener('click', function (e) {
@@ -32,6 +38,33 @@ export function resetInput() {
 
 export function showInfo(data) {
   data.then(response => UI_ELEMENTS.TEMPERATURE.textContent = Math.round(response.main.temp - 273.16) + '°')
-  data.then(response => UI_ELEMENTS.CITY.forEach(city => city.textContent = response.name))
+  data.then(response => UI_ELEMENTS.CITY.forEach(city => {
+    city.textContent = response.name;
+    const checkCity = favoriteCity.find(elem => elem === response.name)
+    if (checkCity) {
+      UI_ELEMENTS.BUTTON_LIKE.classList.add('main__btn-heart--active');
+    } else {
+      UI_ELEMENTS.BUTTON_LIKE.classList.remove('main__btn-heart--active');
+    }
+  }))
   data.catch(err => console.log('hi', err))
+}
+
+export function addFavoriteCite(nameCity) {
+  UI_ELEMENTS.LIST_FAVORITE_CITY.insertAdjacentHTML('beforeend',
+    `<li class="main__item">
+      <button class="main__btn" type="button">${nameCity}</button>
+      <button class="main__delete" type="button"></button>
+    </li>`
+  )
+}
+
+export function deleteFavoriteCity(nameCity) {
+  document.querySelectorAll('.main__btn').forEach(
+    elem => {
+      if (elem.textContent === nameCity) {
+        elem.parentElement.remove()
+      }
+    }
+  )
 }
